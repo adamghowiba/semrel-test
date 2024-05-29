ce963ea68d4d1c6606487fefad989e076007b0e5
Adam <adamware99@hotmail.com>
2024-05-30 01:38:36 +0400

    chore: another change here

--- apps/rest-demo/src/main.ts
+++ apps/rest-demo/src/main.ts
@@ -9,6 +9,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 // Some major change
+// Another some
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
