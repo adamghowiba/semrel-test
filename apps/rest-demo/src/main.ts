579ca7c947c41690ed1eaeb6185f578b8ba0d6f6
Adam <adamware99@hotmail.com>
2024-05-30 04:03:56 +0400

    feat: added a new feature

--- apps/rest-demo/src/main.ts
+++ apps/rest-demo/src/main.ts
@@ -13,6 +13,8 @@ import { AppModule } from './app/app.module';
 // did some mroe fixing
 // Added a new featurea
 // Added 2 fix
+// Added a new feature
+// Added a bug fix
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
