d7383c25e88e9dcb112e257c1724771a9c5ce02f
Adam <adamware99@hotmail.com>
2024-05-31 04:10:29 +0400

    fix: did something

--- apps/rest-demo/src/main.ts
+++ apps/rest-demo/src/main.ts
@@ -2,22 +2,9 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
-
-// Test
-// Test
-// did some mroe fixing
-// Added a new featurea
-// Added 2 fix
-// Add a feature
-// Fixing
-// Another fix
-// Added a new feature
-// Added on more feature
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
